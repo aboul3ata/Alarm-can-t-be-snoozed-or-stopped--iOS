49735c12ed68c2f3f17d223ff8ba79f658ed170f
Ali Abouelatta <ata311@nyu.edu>
2017-07-11 13:34:49 +0200

    ConfigureCell
    did more migration and configure Cell function to display appropriate
    info(alarm title + img)

--- Annoying Alarm- Alarm can't be snoozed or stopped!/addAlarmVC.swift
+++ Annoying Alarm- Alarm can't be snoozed or stopped!/addAlarmVC.swift
@@ -17,10 +17,11 @@ class addAlarmVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
     
     private var _durationArray = ["30 seconds", "60 seconds" ,"90 seconds","2 minutes"
     ,"3 minutes", "4 minutes","5 minutes" , "7 minutes", "10 minutes", "15 minutes"]
-    private var _timeofAlarm:String!
+    private var _timeofAlarmTitle:String!
     private var _warningofAlarm:Bool!
     private var _annoyingAlarm:Bool!
     private var _durationAlarm:Double!
+    private var _timeofAlarm:Date!
     
     
     override func viewDidLoad() {
@@ -30,8 +31,12 @@ class addAlarmVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
     }
 
     
-    func timeChanged(_ sender: UIDatePicker) -> String {
-        let alarmTime = sender.date //returns date in GMT so needs to be formatted
+    func timeChanged(_ sender: UIDatePicker) -> Date {
+                        //returns date in GMT so needs to be formatted
+        return sender.date
+    }
+    
+    func getTimeString(alarmTime: Date) -> String{
         let formatter = DateFormatter()
         formatter.dateFormat = "h:mm a"
         formatter.timeZone = TimeZone.current
@@ -59,15 +64,16 @@ class addAlarmVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
     //calling all functions above upon submission
     @IBAction func confirm(_ sender: Any) {
         let timeofAlarm =  timeChanged(alarmTime)
+        let timeofAlarmTitle = getTimeString(alarmTime: timeofAlarm)
         let warningofAlarm = warningBOL.isOn
         let annoyingAlarm = annoyingAlarmBOL.isOn
         let durationOfAlarm = durationPicker.selectedRow(inComponent: 0)
-        
         self._timeofAlarm = timeofAlarm
+        self._timeofAlarmTitle = timeofAlarmTitle
         self._warningofAlarm = warningofAlarm
         self._annoyingAlarm = annoyingAlarm
         self._durationAlarm = Double(durationOfAlarm)
-        print("\(timeofAlarm) \(warningofAlarm) \(annoyingAlarm ) \(durationOfAlarm)")
+        print("\(timeofAlarm) \(warningofAlarm) \(annoyingAlarm ) \(durationOfAlarm) is annoying \(annoyingAlarm)")
         saveNewAlarmCoreData()
     }
     
@@ -75,8 +81,9 @@ class addAlarmVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
         let newAlarm = Alarm(context:context)
         newAlarm.warning = self._warningofAlarm
         newAlarm.duration = self._durationAlarm
-//        newAlarm.time = self._timeofAlarm
-
+        newAlarm.time = self._timeofAlarm as NSDate?
+        newAlarm.annoying = self._annoyingAlarm
+        newAlarm.timeTitle = self._timeofAlarmTitle
         ad.saveContext()
     }
 
