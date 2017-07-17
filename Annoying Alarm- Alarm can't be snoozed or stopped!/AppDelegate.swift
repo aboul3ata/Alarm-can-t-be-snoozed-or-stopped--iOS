1e5a67b5321ac39b8ac179100bac9fd057dcff20
Ali Abouelatta <ata311@nyu.edu>
2017-07-17 23:06:22 +0200

    New logo
    -

--- Annoying Alarm- Alarm can't be snoozed or stopped!/AppDelegate.swift
+++ Annoying Alarm- Alarm can't be snoozed or stopped!/AppDelegate.swift
@@ -27,13 +27,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        if player?.isPlaying == true {
+            
+            let url = Bundle.main.url(forResource: "oldClock", withExtension: "wav")!
+            
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                guard let player = player else { return }
+                
+                player.prepareToPlay()
+                player.volume = 1
+                player.numberOfLoops = 3 
+                player.play()
+            } catch let error as NSError {
+                
+            }
+            
+            
+        }
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+ 
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -156,6 +173,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
             guard let player = player else { return }
             
             player.prepareToPlay()
+            player.volume = 1
             player.play()
         } catch let error as NSError {
             
